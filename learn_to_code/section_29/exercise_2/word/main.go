5bfacd3f439f4df80e266183cbe7727f3b0ec6da
Alex Livingston <alexplivings@gmail.com>
2019-12-10 09:04:16 -0500

    Added some documentation, removed c.out file

--- learn_to_code/section_29/exercise_2/word/main.go
+++ learn_to_code/section_29/exercise_2/word/main.go
@@ -1,9 +1,9 @@
+// Package word provides functions for finding/counting/etc. words
 package word
 
 import "strings"
 
-// no need to write an example for this one
-// writing a test for this one is a bonus challenge; harder
+// UseCount returns the number of times each word is used
 func UseCount(s string) map[string]int {
 	xs := strings.Fields(s)
 	m := make(map[string]int)
@@ -13,6 +13,7 @@ func UseCount(s string) map[string]int {
 	return m
 }
 
+// Count returns the number of words in a string
 func Count(s string) int {
 	// write the code for this func
 	xs := strings.Fields(s)
